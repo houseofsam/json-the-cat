6c8d69122572d465a9170a15a32ba8e97dc617de
houseofsam <samxsaid@gmail.com>
2021-12-03 02:11:00 +0000

    breedFetcher.js cat api

---
+++ breedFetcher.js
@@ -0,0 +1,20 @@
+const request = require('request');
+const args = process.argv.slice(2);
+
+request(`https://api.thefcatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response && response.statusCode);
+    
+    const data = JSON.parse(body);
+    // console.log(data);
+    // console.log(typeof data);
+  
+    if (data.length === 0) {
+      console.log('Breed not found!');
+    } else {
+      console.log(data[0].description);
+    }
+  }
+});
