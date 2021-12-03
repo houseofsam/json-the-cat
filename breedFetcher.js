bbd1f6d77f10b52e9a4daf4ad8542a7d0b9af70d
houseofsam <samxsaid@gmail.com>
2021-12-03 02:16:13 +0000

    edit

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const args = process.argv.slice(2);
 
-request(`https://api.thefcatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
