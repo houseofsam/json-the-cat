218b9f0073b34cf3fce8bf7ef335a108cf2f8de7
houseofsam <samxsaid@gmail.com>
2021-12-03 03:16:58 +0000

    successfully refactored fetchBreedDescription function and imported it into index.js with a callback function either displaying a result or an error

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,27 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(response && response.statusCode);
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    
+    callback(null, response && response.statusCode);
     
     const data = JSON.parse(body);
-    // console.log(data);
-    // console.log(typeof data);
-  
+
     if (data.length === 0) {
-      console.log('Breed not found!');
+      callback('Breed not found!', null);
     } else {
-      console.log(data[0].description);
+      callback(null, data[0].description);
     }
-  }
-});
+  });
+
+};
+
+
+module.exports = { fetchBreedDescription };
+
+
